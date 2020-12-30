e6486c6653abe1932dafdbd0ea2b795c7eaa5087
Natasha <natasha.katson@gmail.com>
2020-12-30 16:08:40 -0500

    init commit

---
+++ content/posts/hello-world.md
@@ -0,0 +1,6 @@
+---
+title: "Hello World"
+date: 2020-12-29T15:44:13-05:00
+draft: true
+---
+
---
+++ themes/harbor/webpack.common.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+module.exports = {
+    entry: {"main.js" : ['./static/src/main.js', './static/src/theme.js']},
+    output: {
+        path: `${__dirname}/static/js/`,
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css/,
+            use: [
+              "style-loader",
+              {
+                loader: "css-loader",
+                options: { url: false }
+              }
+            ]
+          }
+        ]
+      }
+};
