c9d4c864fe44211cf0b23b8c6e47ed23129b3f36
Natasha <natasha.katson@gmail.com>
2021-01-02 21:41:13 -0500

    new script

--- deploy.sh
+++ deploy.sh
@@ -1,39 +1,31 @@
+
 #!/bin/sh
 
-# If a command fails then the deploy stops
-set -e
+git add .
 
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
 
+echo "Deleting old publication"
 rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
 
-git add .
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public upstream/gh-pages
 
-# Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
-git commit -m "$msg"
+echo "Removing existing files"
+rm -rf public/*
 
-# Push source and build repos.
-git push origin main
+echo "Generating site"
+hugo
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
-# Go To Public folder
-cd public
-
-# Add changes to git.
-git add .
-
-# Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin main
+echo "Pushing to github"
+git push --all
